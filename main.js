473cf3749b392e69c43f33d63dbf4cc9b0c49c91
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-14 09:34:22 -0500

    feat: :sparkles: creacon del objeto Campus

---
+++ main.js
@@ -0,0 +1,19 @@
+const campusLan = {
+    campus : {
+        Bucaranga : [],
+        Bogota:[],
+        Medellin:[],
+        Mexico:[]
+    },
+    trainers : "",
+    campers : "",
+    niveles : "",
+    tecnologias : "",
+    teams : "",
+    roadMap : ""
+}
+
+
+
+console.log(campusLan.campus.Bogota)
+
