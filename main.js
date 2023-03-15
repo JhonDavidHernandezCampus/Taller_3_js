c562fa2634ee3ee05d14a5b4ef2fe9dcad498689
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-15 13:46:27 -0500

    feat: :art: Se realizo un abance en myFormularioPersonas

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let campus = {};
 myFormularioCampus.addEventListener("submit",(e)=>{
     e.preventDefault();/* para que el formulario no me redireccione a otra pagina */
     let data = Object.fromEntries(new FormData(e.target));
-    campus[`${data.nombreSede}`] = []
+    campus[`${data.nombreSede}`] = {Camper: [],trainer: []};
     console.log(campus);
     listaSedes();
 
     myFormularioCampus.reset()
 })
-
+3
 
 let listaSedes = ()=>{
 let opciones = document.querySelector("[name='Sede']")
@@ -24,11 +24,19 @@ let opciones = document.querySelector("[name='Sede']")
         `)        
     }
 
-    console.log(Object.entries(campus));/* me convierte un objetop a un araylist */
+    console.log(Object.entries(campus));/* me convierte un objeto a un araylist */
 
 }
 
-myFormularioPersonas
+myFormularioPersonas.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target))
+    console.log(data)
+    campus[`${data.Sede}`]["camper"].unshift(data);
+    console.log(campus);
+
+    myFormularioPersonas.reset();
+})
 
 
 
