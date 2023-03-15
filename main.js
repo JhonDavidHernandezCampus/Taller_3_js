2b94e844755c5743bd8f0eea6d3b9ea3028c7a12
JhonDavidHernandezCampus <yonif975@gamil.com>
2023-03-15 09:37:33 -0500

    feat: :art: Inicio de formulario con DOM

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-const campusLan = {
-    campus : {
-        Bucaranga : [],
-        Bogota:[],
-        Medellin:[],
-        Mexico:[]
-    },
-    trainers : "",
-    campers : "",
-    niveles : "",
-    tecnologias : "",
-    teams : "",
-    roadMap : ""
+let myFormularioCampus = document.querySelector("#myFormularioCampus")
+let myFormularioPersonas = document.querySelector("#myFormularioPersonas")
+
+let campus = {};
+
+myFormularioCampus.addEventListener("submit",(e)=>{
+    e.preventDefault();/* para que el formulario no me redireccione a otra pagina */
+    let data = Object.fromEntries(new FormData(e.target));
+    campus[`${data.nombreSede}`] = []
+    console.log(campus);
+    listaSedes();
+
+    myFormularioCampus.reset()
+})
+
+
+let listaSedes = ()=>{
+let opciones = document.querySelector("[name='Sede']")
+
+    for (let [val , id] of Object.entries(campus)) {
+        console.log(id);
+        opciones.insertAdjacentHTML("beforeend", `
+        <option value="${val}">${val}</option>
+        `)        
+    }
+
+    console.log(Object.entries(campus));/* me convierte un objetop a un araylist */
+
 }
 
+myFormularioPersonas
 
 
-console.log(campusLan.campus.Bogota)
+
+
 
