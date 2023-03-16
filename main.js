61f4aad83cc0d5a228fa05e15ebc6974235d17d7
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-03-16 09:38:40 -0500

    feat: :art: areglo de myFormularioPersonas

--- main.js
+++ main.js
@@ -1,42 +1,55 @@
 let myFormularioCampus = document.querySelector("#myFormularioCampus")
 let myFormularioPersonas = document.querySelector("#myFormularioPersonas")
 
-let campus = {};
+let campus = {sedes:[]
+
+};
+
 
 myFormularioCampus.addEventListener("submit",(e)=>{
     e.preventDefault();/* para que el formulario no me redireccione a otra pagina */
     let data = Object.fromEntries(new FormData(e.target));
-    campus[`${data.nombreSede}`] = {Camper: [],trainer: []};
-    console.log(campus);
-    listaSedes();
+    campus[`${data.nombre_de_Sede}`] = { "Campers":[]
+    };
 
+
+    console.log(campus);
+
+    listaSedes('[name="Sede"]', campus);
+
+    console.log("Objeto",Object.fromEntries(new FormData(e.target)));
     myFormularioCampus.reset()
 })
-3
 
-let listaSedes = ()=>{
-let opciones = document.querySelector("[name='Sede']")
 
-    for (let [val , id] of Object.entries(campus)) {
-        console.log(id);
-        opciones.insertAdjacentHTML("beforeend", `
+let listaSedes = (p1,p2)=>{
+let opciones = document.querySelector(p1)
+    for (let [val , id] of Object.entries(p2)) {/* el parametro 2 es donde voy a ingressar el dato  que se encuentra en el parametro 1*/
+        opciones.insertAdjacentHTML("beforeend", `+
         <option value="${val}">${val}</option>
         `)        
     }
 
     console.log(Object.entries(campus));/* me convierte un objeto a un araylist */
-
 }
 
 myFormularioPersonas.addEventListener("submit",(e)=>{
+    console.log("..");
+    console.log("..");
     e.preventDefault();
-    let data = Object.fromEntries(new FormData(e.target))
-    console.log(data)
-    campus[`${data.Sede}`]["camper"].unshift(data);
-    console.log(campus);
+    let data = Object.fromEntries(new FormData(e.target));
+    let sede = data.Sede;
 
-    myFormularioPersonas.reset();
+    console.log(sede);
+    console.log(data.nombre_de_Sede);
+    delete data.sede;                       /* quitar propiedad de un objeto */
+
+    campus[`${sede}`]["Campers"].unshift(data); 
+
+    myFormularioPersonas.reset()
 })
+
+console.log(campus)
 
 
 
